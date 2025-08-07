74c69b4fb4ab09a86aaba5bc2eb58c8904e6bde9
Nerotas <Nerotas7@gmail.com>
2025-08-07 15:48:29 -0500

    Update layout.tsx

--- FrontEnd/src/app/(DashboardLayout)/layout.tsx
+++ FrontEnd/src/app/(DashboardLayout)/layout.tsx
@@ -66,4 +66,24 @@ const RootLayout = ({ children }: Props) => {
   );
 };
 
+export const metadata = {
+  title: 'Tough Nose Karate',
+  description:
+    'Tough Nose Karate provides essential resources and management tools for Tang Soo Do students and instructors.',
+  openGraph: {
+    title: 'Tough Nose Karate',
+    url: 'https://toughnosekarate.netlify.app/',
+    description: 'Empowering Tang Soo Do students with modern martial arts management.',
+    images: [
+      {
+        url: '/sitepreview.png', // Update with your actual image path
+        width: 300,
+        height: 200,
+        alt: 'Tough Nose Karate Logo',
+      },
+    ],
+    type: 'article',
+  },
+};
+
 export default RootLayout;
