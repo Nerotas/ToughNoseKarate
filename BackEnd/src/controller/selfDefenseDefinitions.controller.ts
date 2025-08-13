7d8c03f3e15548c3d5e27927c33262f883d7e09b
Nerotas <Nerotas7@gmail.com>
2025-08-12 19:00:06 -0500

    keep name

--- BackEnd/src/controller/selfDefenseDefinitions.controller.ts
+++ BackEnd/src/controller/selfDefenseDefinitions.controller.ts
@@ -15,7 +15,7 @@ import { Roles } from 'src/decorators/roles.decorator';
 import { RolesGuard } from 'src/guards/roles.guard';
 
 @ApiTags('Techniques')
-@Controller({ path: 'selfdefense-definitions', version: '1' })
+@Controller({ path: 'self-defense-definitions', version: '1' })
 export class SelfDefenseDefinitionsController {
   constructor(
     private readonly selfDefenseDefinitionsService: SelfDefenseDefinitionsService,
